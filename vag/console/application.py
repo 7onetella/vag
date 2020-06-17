3dfda710edad8a4b2201246f4da6d270664bcae7
Seven Tella <7onetella@gmail.com>
2020-06-16 21:11:36 -0400

    0.1.2

--- vag/console/application.py
+++ vag/console/application.py
@@ -2,6 +2,7 @@ import click
 from os.path import expanduser
 from vag import __version__
 from vag.console.commands.instance import instance
+from jinja2 import Template
 
 
 @click.group()
@@ -17,41 +18,55 @@ def version():
 
 
 @root.command()
-@click.argument('box', default='7onetella/ubuntu-19.10', metavar='<box>')
+@click.argument('box', default='7onetella/ubuntu-20.04', metavar='<box>')
 @click.argument('hostname', default='', metavar='<hostname>')
 @click.argument('ip_address', default='', metavar='<ip_address>')
 @click.option('--interface', default='eno1', help='network interface')
 @click.option('--memory', default='1024', help='memory')
+@click.option('--service', default='', help='service to start')
 @click.option('--debug', is_flag=True, default=False, help='debug this command')
-def init(box, hostname, ip_address, interface, memory, debug):
+def init(box, hostname, ip_address, interface, memory, service, debug):
     """Initializes a new Vagrantfile"""
 
     home = expanduser("~")
 
-    template = f"""
+    template = Template("""
 Vagrant.configure("2") do |config|
 
-  config.vm.box = "{box}"
-  config.vm.box_url = "file://{home}/.vagrant_boxes/{box}/package.box"
+  config.vm.box = "{{ box }}"
+  config.vm.box_url = "file://{{ home }}/.vagrant_boxes/{{ box }}/package.box"
 
-  config.vm.network "public_network", ip: "{ip_address}", bridge: "{interface}"
-
+  config.vm.network "public_network", ip: "{{ ip_address }}", bridge: "{{ interface }}"
+  {% if service|length %}
+  config.vm.provision "shell",
+    run: "always",
+    inline: "systemctl start {{ service }}"
+  {% endif %}
   config.vm.provider "virtualbox" do |vb|
-    vb.name   = "{hostname}"
-    vb.memory = "{memory}"
+    vb.name   = "{{ hostname }}"
+    vb.memory = "{{ memory }}"
   end
 
-  config.vm.hostname = "{hostname}"
+  config.vm.hostname = "{{ hostname }}"
   config.ssh.insert_key = false # 1
   config.ssh.private_key_path = ['~/.vagrant.d/insecure_private_key', '~/.ssh/id_rsa'] # 2
   config.vm.provision "file", source: "~/.ssh/id_rsa.pub", destination: "~/.ssh/authorized_keys" # 3
 
-end"""
+end""")
 
+    output = template.render(
+        box=box,
+        home=home,
+        hostname=hostname,
+        memory=memory,
+        ip_address=ip_address,
+        interface=interface,
+        service=service
+    )
     f = open('./Vagrantfile','w+')
-    f.write(template)
+    f.write(output)
     f.close()
-    print(template)
+    print(output)
 
 
 root.add_command(version)
