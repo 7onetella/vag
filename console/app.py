267f648e983877871b97c54ccc4a94dee704b09a
Seven Tella <7onetella@gmail.com>
2021-06-17 01:04:55 -0400

    fixing password special characters

--- console/app.py
+++ console/app.py
@@ -311,7 +311,7 @@ def create_new_user(google_id: str) -> User:
     pwo.minlchars = 3 # (Optional)
     pwo.minnumbers = 1 # (Optional)
     pwo.minschars = 0 # (Optional)
-    pwo.excludeschars = "!$%^*"
+    pwo.excludeschars = "!$%^*:="
     random_password = pwo.generate()
     random_email = f'{random_username}@example.com'
     return user_util.add_user(f'{random_username}', random_password, random_email, google_id, exitOnFailure=False)
